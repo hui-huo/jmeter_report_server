dfa9a8fb0d8d4e5e5302d8d9f938b36f179c1171
huihuo <xuechao.luan@outlook.com>
2023-04-20 18:40:47 +0800

    修改允许hosts

--- jmeter_report_server/settings.py
+++ jmeter_report_server/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p+^)z%0s6a)psjiz!4u55jnlka^i@^h#^rc5&%&+%0_@@o8h9%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
