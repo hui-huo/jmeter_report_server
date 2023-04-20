1c0e5fc1c9d9ad74e6e1ab5166ea7433a5b08dde
huihuo <xuechao.luan@outlook.com>
2023-04-20 19:51:32 +0800

    add favicon

--- jmeter_report_server/urls.py
+++ jmeter_report_server/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
